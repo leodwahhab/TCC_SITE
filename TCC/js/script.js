efe20a9f8329ba90e455eadd31b5707bf5aea4bb
leodwahhab <leodwahhab@hotmail.com>
2022-08-15 19:44:25 -0300

    atualizacao 3
    - remoção do botão de download do app desktop
    - implementaçao do funcionamento do botão da sidebar e da sidebar
    -implementação de ícones do instagram, facebook e email.

--- TCC/js/script.js
+++ TCC/js/script.js
@@ -60,3 +60,21 @@ window.onscroll = function(){
         document.querySelector('header').style.boxShadow = 'none'
     }
 }
+
+function animarMenu(){
+    let container = document.querySelector('.container')
+    let checkbox = document.getElementById('check')
+    let menu = document.getElementById('menu')
+    let sidebar = document.querySelector('.sidebar')
+    
+    if(checkbox.checked){
+        container.style.opacity = '0.8'
+        sidebar.style.left = '0vw';
+        menu.style.transform = 'rotate(90deg)'
+    }
+    else{
+        sidebar.style.left = '-60vw';
+        container.style.opacity = '1'
+        menu.style.transform = 'rotate(0deg)'
+    }
+}
